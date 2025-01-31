551f6b5e21d7c0a0dd46293c97eeff4d2cdbeaa2
MrImmortal09 <reach.oms2004@gmail.com>
2025-02-01 03:06:15 +0530

    defconfig: added authorship added build script
    Signed-off-by: MrImmortal09 <reach.oms2004@gmail.com>

---
+++ build.sh
@@ -0,0 +1,18 @@
+export PATH="$(pwd)/toolchain/bin:$PATH"
+
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=aarch64-linux-android-
+export CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
+
+# Run defconfig for AOSP and m31
+make O=out LLVM=1 CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip \
+  OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf \
+  HOSTCC=clang HOSTCXX=clang++ HOSTAR=llvm-ar HOSTLD=ld.lld \
+  -j$(nproc) spes-perf_defconfig
+
+# Compile kernel with Clang + LLVM
+make O=out LLVM=1 CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip \
+  OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf \
+  HOSTCC=clang HOSTCXX=clang++ HOSTAR=llvm-ar HOSTLD=ld.lld \
+  -j$(nproc)
